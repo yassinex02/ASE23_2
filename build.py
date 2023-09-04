e8d3e741bc5b8d10ad052d11a6e72ca1e582ef30
Yassine <tahayassinemoumni@gmail.com>
2023-09-04 16:54:27 +0200

    All files created

---
+++ build.py
@@ -0,0 +1,4 @@
+import subprocess
+def build_project():
+subprocess.run(["python", "src/mycode.py"])
+if __name__ == "__main__": build_project()
---
+++ src/mycode.py
@@ -0,0 +1,4 @@
+import numpy as np
+def my_function():
+arr = np.array([1, 2, 3]) return np.sum(arr)
+if __name__ == "__main__": print(my_function())
