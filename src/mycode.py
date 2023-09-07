874ce57aa08414f0307536a332bbde46d788f6d4
yassinex02 <123518324+yassinex02@users.noreply.github.com>
2023-09-07 15:43:09 +0100

    Update mycode.py

--- src/mycode.py
+++ src/mycode.py
@@ -1,4 +1,8 @@
 import numpy as np
 def my_function():
-arr = np.array([1, 2, 3]) return np.sum(arr)
+    arr = np.array([1, 2, 3]) 
+    return np.sum(arr)
+
+
+
 if __name__ == "__main__": print(my_function())
